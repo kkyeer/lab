8ff34e2077e533125fde2dda92ca3f56ff9b82c6
kkyeer <kkyeer@gmail.com>
2021-12-18 23:28:12 +0800

    log4j2 rce

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# LAB
+# Log4j2 远程执行漏洞
+
+
+
+1. 运行com.kkyeer.exploit.log4j2.rce.rmi.RegistryService
+2. 开启http服务，根目录设定为evil-rmi/target/classes
+3. 运行RCE
---
+++ evil-rmi/src/main/java/EvilPrinter.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: kkyeer
+ * @Description:
+ * @Date:Created in 下午5:48 2021/12/12
+ * @Modified By:
+ */
+public class EvilPrinter {
+    static {
+        System.out.println("You are exploited!!!!");
+        System.out.println("This is where evil code");
+    }
+}
