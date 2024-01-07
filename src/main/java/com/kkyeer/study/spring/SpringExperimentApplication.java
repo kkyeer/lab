f40244738fdd7bf23a21c1f669ee881eb25184c5
kkyeer <kkyeer@gmail.com>
2024-01-07 22:30:49 +0800

    explore: MetaSpaceFullGC复现代码

--- src/main/java/com/kkyeer/study/spring/SpringExperimentApplication.java
+++ src/main/java/com/kkyeer/study/spring/SpringExperimentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @Author: kkyeer
  * @Description: Application服务类
+ * 启动参数:  -XX:MaxMetaspaceSize=33270K -XX:+PrintGCDetails -XX:+UseConcMarkSweepGC -Xmx1g -Xms1g
  * @Date:Created in 下午9:57 2022/11/12
  * @Modified By:
  */
--- src/main/java/com/kkyeer/study/spring/controller/DemoController.java
+++ src/main/java/com/kkyeer/study/spring/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.kkyeer.study.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
 
-    @GetMapping("/ping")
-    public String pingPong(){
+    @PostMapping("/ping")
+    public String pingPong(@RequestBody String bodyString) {
+        for (int i = 0; i < 100; i++) {
+            if (i > 15) {
+                System.out.println(i + "-" + bodyString);
+            }
+        }
+        return "pong";
+    }
+
+    @PostMapping("/ping2")
+    public String pingPong2(@RequestBody TestDTO bodyObj) {
+        for (int i = 0; i < 20; i++) {
+            TestDTO copied = new TestDTO();
+            BeanUtils.copyProperties(bodyObj, copied);
+            if (i > 15) {
+                System.out.println(i+"-"+copied.getF2());
+            }
+        }
+        return "pong";
+    }
+    @PostMapping("/ping3")
+    public String pingPong3(@RequestBody TestDTO2 bodyObj) {
+        for (int i = 0; i < 20; i++) {
+            TestDTO2 copied = new TestDTO2();
+            copied.setF1(bodyObj.getF1());
+            copied.setF2(bodyObj.getF2());
+            if (i > 15) {
+                System.out.println(i+"-"+copied.getF2());
+            }
+        }
         return "pong";
     }
 }
